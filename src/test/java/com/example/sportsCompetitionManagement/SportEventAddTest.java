48335ed6a4b381dc8d96980a734084603649dfc6
KEYF <khanakin.ilya10@mail.ru>
2023-06-08 01:26:13 +0300

    testing

--- src/test/java/com/example/sportsCompetitionManagement/SportEventAddTest.java
+++ src/test/java/com/example/sportsCompetitionManagement/SportEventAddTest.java
@@ -17,22 +17,22 @@ public class SportEventAddTest {
 
 
     @Test
-    public void getBookName() {
+    public void getEventId() {
         SportEvent event = new SportEvent(1,"соревнование1","место1","2023");
         assertEquals(1, event.getEventId());
     }
     @Test
-    public void getBookAuthor() {
+    public void getName() {
         SportEvent event = new SportEvent(1,"соревнование1","место1","2023");
         assertEquals("соревнование1", event.getName());
     }
     @Test
-    public void getBookGenre() {
+    public void getLocation() {
         SportEvent event = new SportEvent(1,"соревнование1","место1","2023");
         assertEquals("место1", event.getLocation());
     }
     @Test
-    public void getBookPresence() {
+    public void getDate() {
         SportEvent event = new SportEvent(1,"соревнование1","место1","2023");
         assertEquals("2023", event.getDate());
     }
